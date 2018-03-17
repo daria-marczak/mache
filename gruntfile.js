56568f6860a97a20b86efbc08bd50355402e15dc
daria marczak <marczak.ria@gmail.com>
2018-03-17 19:15:39 +0100

    Fixed RWD issues

--- gruntfile.js
+++ gruntfile.js
@@ -1,31 +1,77 @@
 module.exports = function(grunt) {
-  
-    //Configuration
+
+    //Project configuration.
     grunt.initConfig({
-      sass: {                              
-        dist: {                             
-          options: {                        
-            style: "expanded"
-          },
-          files: {                         
-            "style.css": "style.scss"
-          }
-        }
-      },
-      watch: {
-        scripts: {
-            files: ["**/*.scss"],
-            tasks: ["sass"],
+        sass: {
             options: {
-                spawn: false,
+                sourceMap: true
             },
-        }
-    }
+            dist: {
+                files: {
+                    "css/main.css" : "sass/main.sass"
+                }
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/build/'
+                }]
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ["sass/*.sass"],
+                tasks: ["sass"],
+                options: {
+                    spawn: false,
+                }
+            }
+        },
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        "app/css/*.css",
+                        "app/*.html"
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: "./app"
+                }
+            }
+        },
+
+        px_to_rem: {
+            dist: {
+              options: {
+                base: 16,
+                fallback: false,
+                fallback_existing_rem: false,
+                ignore: [],
+                map: false
+              },
+              files: {
+                'dest/style.css': ['src/style.css']
+              }
+            }
+          }
     });
-  
-    //Load plugins
-    grunt.loadNpmTasks("grunt-contrib-sass");
+
+    //Load the plugins tasks
+    grunt.loadNpmTasks("grunt-sass");
+    grunt.loadNpmTasks("grunt-contrib-imagemin");
     grunt.loadNpmTasks("grunt-contrib-watch");
-    //Register tasks
-    grunt.registerTask("default", ["sass", "watch"]);
-  };
+    grunt.loadNpmTasks("grunt-browser-sync");
+    grunt.loadNpmTasks('grunt-px-to-rem');
+
+    //Default task(s).
+    grunt.registerTask("default", ["sass", "watch", "browserSync", "px_to_rem"]);
+};
